cce76b34c52a9663188ebb0a763053b824b65e78
Shudd3r <q3.shudder@gmail.com>
2018-02-28 23:00:25 +0100

    Ignore some code sniffer rules for tests

--- tests/FileFixerTest.php
+++ tests/FileFixerTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Polymorphine/CodeStandards package.
+ *
+ * (c) Shudd3r <q3.shudder@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Polymorphine\CodeStandards\Tests;
 
 use PHPUnit\Framework\TestCase;
@@ -7,38 +16,43 @@ use PHPUnit\Framework\TestCase;
 
 class FileFixerTest extends TestCase
 {
-    private function fixFile($file) {
-        $tmpFilename = substr(basename($file), strpos(basename($file), '-') + 1);
-        $tmpFile = dirname(__DIR__) . '/temp/' . $tmpFilename;
-        copy($file, $tmpFile);
-        $executable = dirname(__DIR__) . '/vendor/friendsofphp/php-cs-fixer/php-cs-fixer';
-        $config = dirname(__DIR__) . '/cs-fixer.php.dist';
-        $command = 'fix -v --config=' . $config .' --using-cache=no --path-mode=intersection "' . $tmpFile . '"';
-        echo shell_exec('php ' . $executable . ' ' . $command);
-        $fixed = file_get_contents($tmpFile);
-        unlink($tmpFile);
-        return $fixed;
-    }
-
     /**
      * @dataProvider fileList
+     *
      * @param $expected
      * @param $given
      */
-    public function testFixedFiles_MatchExpectations($expected, $given) {
+    public function testFixedFiles_MatchExpectations($expected, $given)
+    {
         $result = $this->fixFile($given);
         $this->assertSame(file_get_contents($expected), $result);
     }
 
-    public function fileList() {
+    public function fileList()
+    {
         $files = [];
-        foreach (array_diff(scandir(__DIR__ . '/Files'), array('..', '.')) as $file) {
-            list($type, $index) = explode('-', $file, 2) + [false, false];
+        foreach (array_diff(scandir(__DIR__ . '/Files'), ['..', '.']) as $file) {
+            [$type, $index] = explode('-', $file, 2) + [false, false];
             $id = ($type === 'expected') ? 0 : 1;
             isset($files[$index]) or $files[$index] = [];
             $files[$index][$id] = __DIR__ . '/Files/' . $file;
         }
 
         return $files;
+    }
+
+    private function fixFile($file)
+    {
+        $tmpFilename = substr(basename($file), strpos(basename($file), '-') + 1);
+        $tmpFile = dirname(__DIR__) . '/temp/' . $tmpFilename;
+        copy($file, $tmpFile);
+        $executable = dirname(__DIR__) . '/vendor/friendsofphp/php-cs-fixer/php-cs-fixer';
+        $config = dirname(__DIR__) . '/cs-fixer.php.dist';
+        $command = 'fix -v --config=' . $config . ' --using-cache=no --path-mode=intersection "' . $tmpFile . '"';
+        echo shell_exec('php ' . $executable . ' ' . $command);
+        $fixed = file_get_contents($tmpFile);
+        unlink($tmpFile);
+
+        return $fixed;
     }
 }
